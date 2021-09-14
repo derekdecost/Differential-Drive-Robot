ab1305dd14878bb72fa1e3b0f63ee975660ea239
Derek DeCost <decostderek@gmail.com>
2021-09-13 22:48:52 -0400

    Adding HW2 packages for the fibonacci publisher node to the project. Publisher node was tested and verified to be working.
    Signed-off-by: Derek DeCost <decostderek@gmail.com>

---
+++ packages/ros_pkg_hw2/src/hw2_node.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+
+class FibonacciTalker:
+    def __init__(self):
+        self.pub = rospy.Publisher('input', Float32, queue_size=10)
+        
+        # Initial Fibonacci values
+        self.fib1 = 0
+        self.fib2 = 0
+
+    def talk(self):
+        input_val = 0
+
+        # Construct the fibonacci sequence.
+        # On the first pass, the value will be zero (0), then self.fib2 is
+        # assigned a value of one (1). This assignment allows for the 
+        # sequence to continue.
+        if self.fib2 == 0:
+            input_val = self.fib2
+            self.fib2 = 1
+        # As the publishing continues, the fibonacci value will be calculated 
+        # and the values will be assigned as such:
+        # F1 = F2
+        # F2 = F1 + F2
+        else:
+            input_val = self.fib1 + self.fib2
+            self.fib1 = self.fib2
+            self.fib2 = input_val 
+
+        # Output the value to the terminal, write it to the logfile, and write
+        # it to rosout
+        rospy.loginfo(input_val)
+
+        # Publish the fibonacci value to the topic 'input'
+        self.pub.publish(input_val)
+
+if __name__ == '__main__':
+    try:
+        # Register the node with ROS
+        rospy.init_node('hw2_node', anonymous=True)
+
+        # Initialize the Fibonacci talker node
+        fib_talker  = FibonacciTalker()
+
+        # Set the sleep rate to 1Hz
+        rate        = rospy.Rate(1)
+
+        # Run the talker node until ROS exits
+        while not rospy.is_shutdown():
+            fib_talker.talk()
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        pass
