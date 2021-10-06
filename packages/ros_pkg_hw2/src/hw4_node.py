698475863bd4b6bc84910b6dafafa6d95ed736d5
Derek DeCost <decostderek@gmail.com>
2021-10-05 22:49:35 -0400

    *Committing before submitting assignment.
    Signed-off-by: Derek DeCost <decostderek@gmail.com>

--- packages/ros_pkg_hw2/src/hw4_node.py
+++ packages/ros_pkg_hw2/src/hw4_node.py
@@ -2,10 +2,11 @@
 
 import  rospy
 from    mystery_package.msg import UnitsLabelled
+from    std_msgs.msg        import Float32
 
 class FeetConverter:
     def __init__(self, input_topic, output_topic, input_type, output_type, queue_val):
-    	# Create the publisher and subscriber components of the node.
+        # Create the publisher and subscriber components of the node.
         rospy.Subscriber(input_topic, input_type, self.callback)
         self.pub = rospy.Publisher(output_topic, output_type, queue_size=queue_val)
         
@@ -14,39 +15,30 @@ class FeetConverter:
 
         if not rospy.has_param("/hw4/conversion_type"):
             exit
-
-        # Set the conversion unit of of the node based on the configured parameter
-        if rospy.get_param("/hw4/conversion_type") == "meter":
-            self.pub_msg.units = "meters"
-        elif rospy.get_param("/hw4/conversion_type") == "feet":
-            self.pub_msg.units = "feet"
-        elif rospy.get_param("/hw4/conversion_type") == "Smoot":
-            self.pub_msg.units = "Smoot"
         else:
-            # Default to convert to meters if no unit has been specified
-            self.pub_msg.units = "meter"
+            self.units = rospy.get_param("/hw4/conversion_type")
 
     def callback(self, msg):
         # Based on the unit type set by the parameter /hw4/conversion_type, perform
         # a unit conversion and return its value to self.pub_msg.value
-    	if self.pub_msg.units == "meter":
-            self.pub_msg.value = msg.value
-        elif self.pub_msg.units == "feet":
-            self.pub_msg.value = self.meter2feet(msg.value)
-        elif self.pub_msg.value == "Smoot":
-            self.pub_msg.value = self.meter2smoot(msg.value)
+        if self.units == "meter":
+            self.pub_msg.value = msg.data
+        elif self.units == "feet":
+            self.pub_msg.value = self.meter2feet(msg.data)
+        elif self.units == "Smoot":
+            self.pub_msg.value = self.meter2smoot(msg.data)
         
         # Output the value of conversion to the publisher's topic.
         # This occurs as a part of the callback of the subscriber.
         # Once the subscriber receives a value, it will immediately convert the value from
         # meters to feet, and output the value to the log and the publisher's topic.
-        self.pub.publish(self.pub_msg)
-        rospy.loginfo(f"/ft_measurement/output published {self.pub_msg.value} {self.pub_msg.units}")
+        self.pub.publish(Float32(self.pub_msg.value))
+        rospy.loginfo(f"hw4_node published {self.pub_msg.value} {self.pub_msg.units} to topic /mystery/input")
         return
 
     def meter2feet(self, val):
-    	# Perform the conversion of meter's to feet.
-    	# 1 meter = 3.28084 feet, Source: Google.com searching "meters to feet"
+        # Perform the conversion of meter's to feet.
+        # 1 meter = 3.28084 feet, Source: Google.com searching "meters to feet"
         return (val * 3.28084)  
     
     def meter2smoot(self, val):
@@ -57,10 +49,10 @@ class FeetConverter:
 if __name__ == "__main__":
     # Initialize the node and start the subscriber-publisher node.
     rospy.init_node('hw3_node', anonymous=True)
-    FeetConverter(input_topic="/mystery/output2", 
-                  output_topic='/ft_measurement/output', 
-                  input_type=UnitsLabelled, 
-                  output_type=UnitsLabelled, 
+    FeetConverter(input_topic='/hw4/hw4_input', 
+                  output_topic='/mystery/input', 
+                  input_type=Float32, 
+                  output_type=Float32, 
                   queue_val=10)
     rospy.spin()
 
