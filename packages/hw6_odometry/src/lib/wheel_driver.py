86cfd5ac5ff517e83f5421f08ffe685da5e0488d
Derek DeCost <decostderek@gmail.com>
2021-11-02 09:43:14 -0400

    *Tested state changing operation. *Tested motor driving (still needs to be calibrated and tested in lab). *Updated message construction to include the header section.
    Signed-off-by: Derek DeCost <decostderek@gmail.com>

--- packages/hw6_odometry/src/lib/wheel_driver.py
+++ packages/hw6_odometry/src/lib/wheel_driver.py
@@ -3,64 +3,36 @@
 import rospy
 import numpy as np
 
-from duckietown_msgs.msg import WheelsCmdStamped
 from duckietown_msgs.msg import Twist2DStamped
 from odometry_hw.msg     import DistWheel
 
-WHEEL_CMD_TOPIC = "/beezchurger/lane_controller_node/car_cmd"
+WHEEL_CMD_TOPIC = "/beezchurger/car_cmd_switch_node/cmd"
 WHEEL_CMD_TYPE  = Twist2DStamped
 
 ROBOT_AXLE_LENGTH = 0.1
 
 class WheelDriver:
-    def __init__(self, parent, pause_rate=0.5):
-        # rospy.init_node(f"wheel_driver_{parent}", anonymous=True)
+    def __init__(self, pause_rate=0.5):
         rospy.Subscriber("/lab2/wheel_distance_delta", DistWheel, self.__cb_update_distance_traveled)
         self.wheel_twist_pub = rospy.Publisher(WHEEL_CMD_TOPIC, WHEEL_CMD_TYPE, queue_size=10)
-        
-        # Create the velocity setting message and the initial values
-        # self.wheel_velocity            = WheelsCmdStamped()
-        # self.wheel_velocity.header.seq = 1800
-        # self.wheel_velocity.header.stamp.secs = 0
-        # self.wheel_velocity.header.stamp.nsecs = 0
-        # self.wheel_velocity.frame_id = ''
-        # self.wheel_velocity.vel_left   = 0
-        # self.wheel_velocity.vel_right  = 0
-        # self.wheel_velocity_pub.publish(self.wheel_velocity)
 
         self.wheel_twist       = Twist2DStamped()
+        self.wheel_twist.header.seq         = 0
+        self.wheel_twist.header.stamp.secs  = 0
+        self.wheel_twist.header.stamp.nsecs = 0
+        self.wheel_twist.header.frame_id    = ""
         self.wheel_twist.v     = 0
         self.wheel_twist.omega = 0
         self.l_wheel_distance_traveled = 0
         self.r_wheel_distance_traveled = 0 
-        
         self.pause_rate = rospy.Rate(pause_rate)
 
     def stop(self):
         self.wheel_twist.v     = 0
         self.wheel_twist.omega = 0
         self.wheel_twist_pub.publish(self.wheel_twist)
+        self.__reset_distance_traveled()
         return
-
-    # def drive(self, speed, l_ratio=1, r_ratio=1, l_distance=None, r_distance=None):
-    #     # Set the speed of each wheel
-    #     self.wheel_velocity.vel_left  = l_ratio * speed
-    #     self.wheel_velocity.vel_right = r_ratio * speed
-    #     self.wheel_velocity_pub.publish(self.wheel_velocity)
-
-    #     # If wheel distances are specified, continue to track the distance traveled
-    #     # then, set the robot speed to zero (0).
-    #     if l_distance is None or r_distance is None:
-    #         return
-        
-    #     # Stop the robot's motion after it has traveled the input distance.
-    #     while self.l_wheel_distance_traveled < l_distance and self.r_wheel_distance_traveled < r_distance:
-    #         continue
-        
-    #     self.stop()
-    #     self.__reset_distance_traveled()
-    #     return
-
 
     """
         @fn drive
@@ -74,10 +46,17 @@ class WheelDriver:
 
         @return None.
     """
-    def drive(self, linear_velocity, theta, radius, l_distance=None, r_distance=None):
+    def drive(self, linear_velocity, theta, radius, l_distance=None, r_distance=None, omega=None):
         # Caclulate and publish the twist of the robot.
         self.wheel_twist.v     = linear_velocity
-        self.wheel_twist.omega = (linear_velocity * np.sin(theta)) / radius
+
+        # The method allows for a user to hard set the omega value. If this value is set,
+        # then the twist omega value will be set to the parameter set by the user.
+        if omega is None:
+            self.wheel_twist.omega = (linear_velocity * np.sin(theta)) / radius
+        else:
+            self.wheel_twist.omega = omega
+
         self.wheel_twist_pub.publish(self.wheel_twist)
 
         # If the l_distance or r_distance ketword parameters are not set, return.
@@ -90,58 +69,26 @@ class WheelDriver:
             continue
         
         self.stop()
-        self.__reset_distance_traveled()
         return
 
-    #TODO: Combine the ccw and cw turn_in_place functions
-    def turn_in_place_ccw(self, speed, angle=None):
-        # Calculate the circumferance of the circle to travel.
-        # This circle is formed using the robot's axle length as the diameter.
-        if angle is not None:
-            distance_to_travel = (ROBOT_AXLE_LENGTH / 2) * abs(angle)
+    def turn_in_place(self, speed=8.3, angle=None):
+        if angle is None:
             pass
+        else:
+            distance_to_travel = (ROBOT_AXLE_LENGTH / 2) * abs(angle)
 
-        # Set the speed of each wheel 
-        self.wheel_velocity.vel_left  = -1 * speed
-        self.wheel_velocity.vel_right = speed
-        self.wheel_velocity_pub.publish(self.wheel_velocity)
+        # Set the speed of each wheel.
+        self.wheel_twist.v     = 0
+        self.wheel_twist.omega = speed
+        self.wheel_twist_pub.publish(self.wheel_twist)
 
-        # If the angle of the turn is specified, continue to track the distance traveled
-        # then, set the robot speed to zero (0).
         if angle is None:
             return
-
-        # Stop the robot's turning motion after it has completed the full travel distance
-        while self.l_wheel_distance_traveled < distance_to_travel and self.r_wheel_distance_traveled < distance_to_travel:
-            continue
-
-        self.stop()
-        self.__reset_distance_traveled()
-        return
-
-    def turn_in_place_cw(self, speed, angle=None):
-        # Calculate the circumferance of the circle to travel.
-        # This circle is formed using the robot's axle length as the diameter.
-        if angle is not None:
-            distance_to_travel = (ROBOT_AXLE_LENGTH / 2) * abs(angle)
-            pass
+        else:
+            while self.l_wheel_distance_traveled < distance_to_travel or self.r_wheel_distance_traveled < distance_to_travel:
+                continue 
         
-        # Set the speed of each wheel 
-        self.wheel_velocity.vel_left  = speed
-        self.wheel_velocity.vel_right = -1 * speed
-        self.wheel_velocity_pub.publish(self.wheel_velocity)
-
-        # If the angle of the turn is specified, continue to track the distance traveled
-        # then, set the robot speed to zero (0).
-        if angle is None:
-            return
-        
-        # Stop the robot's turning motion after it has completed the full travel distance
-        while self.l_wheel_distance_traveled < distance_to_travel and self.r_wheel_distance_traveled < distance_to_travel:
-            continue
-
         self.stop()
-        self.__reset_distance_traveled()
         return
 
     def __cb_update_distance_traveled(self, msg):
--- packages/lab2_motor_control/src/fsm_path_driver.py
+++ packages/lab2_motor_control/src/fsm_path_driver.py
@@ -4,7 +4,8 @@ import rospy
 import math
 import sys
 
-from lib.wheel_driver         import WheelDriver
+from lib.wheel_driver    import WheelDriver
+from duckietown_msgs.msg import FSMState
 # from lib.fsm_state_controller import StateController
 
 # All values are in meters unless specified otherwise.
@@ -28,63 +29,89 @@ PATH_RIGHT_LENGTH        = ROBOT_RIGHT_WHEEL_RADIUS * 2 * math.pi
 VELOCITY_RATIO_LEFT      = ROBOT_LEFT_WHEEL_RADIUS  / ROBOT_CENTER_RADIUS
 VELOCITY_RATIO_RIGHT     = ROBOT_RIGHT_WHEEL_RADIUS / ROBOT_CENTER_RADIUS
 
+class DuckiebotFSM:
+    def __init__(self):
+        rospy.Subscriber("/beezchurger/fsm_node/mode", FSMState, self.__cb_duckiebot_fsm)
+        self.fsm_pub            = rospy.Publisher("/beezchurger/fsm_node/mode", FSMState, queue_size=10)
+        self.duckiebot_fsmstate = FSMState()
+        self.duckiebot_fsmstate.header.seq         = 0
+        self.duckiebot_fsmstate.header.stamp.secs  = 0
+        self.duckiebot_fsmstate.header.stamp.nsecs = 0
+        self.duckiebot_fsmstate.header.frame_id    = ""
+        self.duckiebot_fsmstate.state = self.duckiebot_fsmstate.LANE_FOLLOWING
+        self.duckiebot_current_state  = self.duckiebot_fsmstate.NORMAL_JOYSTICK_CONTROL
+        self.fsm_pub.publish(self.duckiebot_fsmstate)
+
+    def publish_state(self, state):
+        self.duckiebot_fsmstate.state = state
+        self.fsm_pub.publish(self.duckiebot_fsmstate)
+        return
+
+    def __cb_duckiebot_fsm(self, msg):
+        self.duckiebot_current_state = msg.state
+        return
+
 if __name__ == "__main__":
     rospy.init_node('fsm_path_driver', anonymous=True)
-    # duckiebot_sc = StateController(parent="fsm_path_driver")
 
     # Get the path type that the robot will travel.
     if not rospy.has_param("/lab2/path_type"):
         sys.exit
-            # StateController.kill_all_nodes()
     else:
         path_type = rospy.get_param("/lab2/path_type")
     
-    duckiebot_wd = WheelDriver("fsm_path_driver")
-    
+    rate = rospy.Rate(1)
+    duckiebot_fsm = DuckiebotFSM()
+    duckiebot_wd  = WheelDriver()
+    rospy.on_shutdown(duckiebot_wd.stop)
+
+    while duckiebot_fsm.duckiebot_current_state != duckiebot_fsm.duckiebot_fsmstate.LANE_FOLLOWING:
+        # Publish the lane following state every second until it changes.
+        duckiebot_fsm.publish_state(duckiebot_fsm.duckiebot_fsmstate.LANE_FOLLOWING)
+        rate.sleep()
+        continue
+
     if path_type == "Line":
         print("Driving in 1m line.")
-        # Drive straight for 1m at 0.25m/s then exit.
-        while not rospy.is_shutdown():
-            duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
+        
+        duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
+        sys.exit()
 
     elif path_type == "Square":
         print("Driving in 1m square")
-        # Drive in a 1m square, then kill all nodes.
-        while not rospy.is_shutdown():
-            # Drive straight for 1m at 0.25m/s.
-            duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
 
-            # Turn in place 90 degrees CCW.
-            duckiebot_wd.turn_in_place_ccw(0.05, angle=(math.pi / 2))
+        # Drive straight for 1m at 0.25m/s.
+        duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
 
-            # Drive straight for 1m at 0.25m/s.
-            duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
+        # Turn in place 90 degrees CCW.
+        duckiebot_wd.turn_in_place(speed=4.0, angle=(math.pi / 2))
 
-            # Turn in place 90 degrees CCW.
-            duckiebot_wd.turn_in_place_ccw(0.05, angle=(math.pi / 2))
+        # Drive straight for 1m at 0.25m/s.
+        duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
 
-            # Drive straight for 1m at 0.25m/s.
-            duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
+        # Turn in place 90 degrees CCW.
+        duckiebot_wd.turn_in_place(speed=4.0, angle=(math.pi / 2))
 
-            # Turn in place 90 degrees CCW
-            duckiebot_wd.turn_in_place_ccw(0.05, angle=(math.pi / 2))
+        # Drive straight for 1m at 0.25m/s.
+        duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
 
-            # Drive straight for 1m at 0.25m/s.
-            duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
+        # Turn in place 90 degrees CCW
+        duckiebot_wd.turn_in_place(speed=4.0, angle=(math.pi / 2))
 
-            # Turn in place 90 degrees CCW.
-            duckiebot_wd.turn_in_place_ccw(0.05, angle=(math.pi / 2))
+        # Drive straight for 1m at 0.25m/s.
+        duckiebot_wd.drive(0.15, 0, 1, l_distance=1, r_distance=1)
 
-            # Stop the duckiebot. The duckiebot should be in its starting position and angle.
-            duckiebot_wd.stop()
+        # Turn in place 90 degrees CCW.
+        duckiebot_wd.turn_in_place(speed=4.0, angle=(math.pi / 2))
 
-            # Kill all nodes upon completion of the 1m square.
-            # duckiebot_sc.kill_all_nodes()
+        # Stop the duckiebot. The duckiebot should be in its starting position and angle.
+        duckiebot_wd.stop()
+        sys.exit()
+            
 
     elif path_type == "Circle":
         print("Driving in 1m diameter circle")
-        while not rospy.is_shutdown():
-            # Drive in a 1m diameter circle at 0.25m/s then kill all nodes.
-            duckiebot_wd.drive(0.15, (math.pi / 2), 0.5, l_distance=PATH_LEFT_LENGTH, r_distance=PATH_RIGHT_LENGTH)
 
+        duckiebot_wd.drive(0.15, (math.pi / 2), 0.5, l_distance=PATH_LEFT_LENGTH, r_distance=PATH_RIGHT_LENGTH)
+        sys.exit()
 
